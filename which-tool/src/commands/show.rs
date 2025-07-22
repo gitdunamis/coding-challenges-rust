f94160c7e217788f4a571e36fec466ef08529cd6
victor <odebode@getrova.com>
2025-07-22 17:56:10 +0100

    - added clap for command line parsing - starting which tool

--- which-tool/src/commands/show.rs
+++ which-tool/src/commands/show.rs
@@ -1,4 +1,22 @@
+use std::fs;
+use std::path::Path;
 
 pub fn show(programs: Vec<String>) {
-    println!("Programs to search for: {}", programs.join(", "))
+    println!("Programs to search for: {}", programs.join(", "));
+    let os_path = std::env::var("PATH").unwrap();
+    println!("System path is: {}", os_path);
+    let paths = os_path.split(':').map(|p| Path::new(p)).collect::<Vec<&Path>>();
+
+    for path in paths {
+        if !path.exists() && !path.is_dir() {
+            continue;
+        }
+        if let Ok(entries) = fs::read_dir(path) {
+            for entry in entries {
+                if let Ok(entry) = entry && entry.path().is_file(){
+                    println!("File Name: {:?} Type: {:?}", entry.file_name(), entry.metadata().unwrap().permissions());
+                }
+            }
+        }
+    }
 }
